3f586cd2c7e4e3bd297b8c53559501233f501217
grandeddie <edvard.radjenovic@gmail.com>
2024-10-06 17:09:51 +0200

    resp nav solved

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+//Navvigation responsive//
+const mobileNav = document.querySelector(".fa-bars");
+const navbar = document.querySelector(".menu");
 
+/*const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("mobileNav-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+*/
